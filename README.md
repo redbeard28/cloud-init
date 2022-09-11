07b242155dd1da5aa27134e6007284b366f738fe
Jéremie CUADRADO <jcuadrado@redbeard-consulting.fr>
2022-09-11 15:21:41 +0200

    Personnalisation de cockpit

---
+++ README.md
@@ -0,0 +1,16 @@
+# cloud-init
+
+ Ce repo me permet de stocker divers scripts permettant à cloud-init d'installer les softs dont j'ai besoin.
+
+Etant un repo public, aucun secrets ne doit être stocké.
+
+# Author
+Jeremie CUADRADO <kum-consulting>
+
+# Version
+Chacun des scripts gères ses propres versions.
+
+# Licence
+Image from cockpit/kumalabs/logo.png belong to the company kuma-consulting.
+
+Only kuma-consulting has the right to use these logs and images
---
+++ cockpit/install_cockpit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Free for All
+# Made with love by Jeremie CUADRADO (kuma-consulting)
+# version: v1
+
+sudo systemctl stop NetworkManager.service
+sudo systemctl disable NetworkManager.service
+
+source /etc/os-release
+if [[ $NAME -eq "Ubuntu" ]]
+  sudo apt install -t ${VERSION_CODENAME}-backports cockpit -y
+  cp -rf kumalabs usr/share/cockpit/branding/
+  sudo systemctl enable cockpit.service
+  sudo echo "ID=kumalabs" >>  /lib/systemd/system/cockpit.service
+  sudo systemctl start cockpit.service
+  sudo apt remove cockpit-networkmanager -y
+fi
