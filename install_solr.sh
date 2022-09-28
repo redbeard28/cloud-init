b85d9fde0f6cc64771d27ad8ef1c193b7ac580ae
kumalabsconsulting <kumalabsconsulting@gmail.com>
2022-09-28 21:49:53 +0200

    Add Debian lsof

--- install_solr.sh
+++ install_solr.sh
@@ -8,7 +8,7 @@ echo "################# SOLR 9.0 INSTALL ################"
 git clone https://github.com/FC-Consulting/formation-solr-tp.git /opt/formation-solr-tp
 useradd -m -d /opt/formation-solr-tp -s /bin/bash solr
 chown -R solr:root /opt/formation-solr-tp
-apt install -y openjdk-17-jdk
+apt install -y openjdk-17-jdk lsof
 echo "################# SOLR 9.0 END ################"
 echo ""
 echo ""
