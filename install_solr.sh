a4d0d684de27a9286ddc4fe32affd3b5b201348e
Jérémie CUADRADO <jeremie@redbeard28-6.local>
2022-09-28 21:05:36 +0200

    Add solr

---
+++ install_solr.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Free for All
+# Made with love by Jeremie CUADRADO (kuma-consulting)
+# version: v1
+echo ""
+echo ""
+echo "################# SOLR 9.0 INSTALL ################"
+
+https://github.com/FC-Consulting/formation-solr-tp.git /opt
+useradd -m -d /opt/formation-solr-tp -s /bin/bash solr
+chown -R solr:root /opt/formation-solr-tp
+echo "################# SOLR 9.0 END ################"
+echo ""
+echo ""
